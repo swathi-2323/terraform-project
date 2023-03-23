78197e193602495f26a4b6ed8aef0424591fc906
swathi-2323 <124771724+swathi-2323@users.noreply.github.com>
2023-03-23 11:03:40 +0530

    tf1

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 #this file consists of code for instances and sg
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIARSPNELGYCJEVYJ4K"
-secret_key = "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"
+access_key = "AKIA46VW7PMOULEHDTH3"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
